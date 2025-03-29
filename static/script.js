a34e0d7320f5c56fd164c318d3c5c886dfaee541
sissizh3n <szhen.zyx@gmail.com>
2025-03-29 11:39:23 -0400

    fixed footer issues

--- static/script.js
+++ static/script.js
@@ -1,4 +1,17 @@
+window.onload = addYear();
+
+
 function toggleDropdown() {
     const dropdown = document.querySelector(".dropdown-content");
     dropdown.classList.toggle("show");
+}
+
+function addYear() {
+    console.log("addYear is running");
+    var currentYear = new Date().getFullYear();
+    document.getElementById("copyYear").innerHTML = currentYear;
+}
+
+function validateForm() {
+    
 }
