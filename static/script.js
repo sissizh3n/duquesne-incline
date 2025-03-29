8799224f2b8b466c8058225942134e68ab8915bb
sissizh3n <szhen.zyx@gmail.com>
2025-03-29 19:36:02 -0400

    mailing list styling, saving before adjusting dropdown styling

--- static/script.js
+++ static/script.js
@@ -13,5 +13,22 @@ function addYear() {
 }
 
 function validateForm() {
-    
+    var inputName = document.getElementById("name");
+    var inputEmail = document.getElementById("email");
+    var inputMsg = document.getElementById("msg");
+    var inputNoMsg = document.getElementById("no-msg");
+    var inputPhoneNum = document.getElementById("phoneNum");
+    var inputZip = document.getElementById("zip");
+
+    var messenge = document.getElementById("inputInvalidMsg");
+
+    if (!inputName.checkValidity()||!inputEmail.checkValidity()||inputMsg.checkValidity()
+        ||inputNoMsg.checkValidity()||inputPhoneNum.checkValidity()||inputZip.checkValidity()) {
+        messenge.innerHTML = "Please fill out the form correctly so we can send updates & info to you!";
+        messenge.style.display = "block";
+        // return false;
+    } else {
+        messenge.style.display = "none";
+        // return true;
+    }
 }
