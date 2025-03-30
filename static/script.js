782c749d1749f9b026c551b91d2a791d63e00515
sissizh3n <szhen.zyx@gmail.com>
2025-03-29 21:15:50 -0400

    added google maps api, check to see if it works

--- static/script.js
+++ static/script.js
@@ -1,9 +1,18 @@
 window.onload = addYear();
-
+if (window.location.href.includes("directions.html")) {
+    initMap();
+}
 
 function toggleDropdown() {
     const dropdown = document.querySelector(".dropdown-content");
     dropdown.classList.toggle("show");
+
+    // if (dropdown.classList.contains("show")) {
+    //     const element = document.getElementById("current-page");  
+    //     element.classList.remove("current-page"); 
+    //     const currElem = document.getElementById("dropdown-nav");
+    //     currElem.classList.add("current-page"); 
+    // }
 }
 
 function addYear() {
@@ -32,3 +41,49 @@ function validateForm() {
         // return true;
     }
 }
+
+
+// async function initMap() {
+//   const position = { lat: 40.443490, lng: -79.941643 };
+//   const { Map } = await google.maps.importLibrary("maps");
+//   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+//   map = new Map(document.getElementById("map"), {
+//     zoom: 10,
+//     center: position,
+//     mapId: "DEMO_MAP_ID",
+//   });
+
+//   const marker = new AdvancedMarkerElement({
+//     map: map,
+//     position: position,
+//     title: "Pittsburgh",
+//   });
+// }
+
+let map;
+
+async function initMap() {
+  // The location of Uluru
+  const position = { lat: 40.439863, lng: -80.017589 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  // The map, centered at Uluru
+  map = new Map(document.getElementById("map"), {
+    zoom: 4,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "Duquesne Incline",
+  });
+}
+
+// initMap();
